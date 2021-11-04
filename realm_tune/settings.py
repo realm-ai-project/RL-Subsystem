9208b5ae51bb91a103be41d9422d7f00830c1862
Ken Ming Lee <leekenming99@gmail.com>
2021-11-04 18:48:10 -0400

    convert from argparse

--- realm_tune/settings.py
+++ realm_tune/settings.py
@@ -1,3 +1,4 @@
+import argparse
 from typing import Dict, Optional, Set, Union
 import warnings
 
@@ -6,9 +7,8 @@ import cattr
 from cattr.converters import Converter
 import wandb
 import yaml
-from mlagents.trainers.settings import EnvironmentSettings, EngineSettings, EnvironmentParameterSettings, CheckpointSettings, TorchSettings
 
-from realm_tune.cli_config import parser
+from realm_tune.cli_config import DetectDefault, parser
 
 @attr.s(auto_attribs=True)
 class WandBSettings:
@@ -18,25 +18,6 @@ class WandBSettings:
     wandb_offline: bool = False
     wandb_group: Union[str, None] = parser.get_default('wandb_group')
     wandb_jobtype: Union[str, None] = parser.get_default('wandb_jobtype')
-
-    # @staticmethod
-    # def structure_from_argparse(obj: Dict, type):
-    #     dict_ = {}
-    #     for k, v in obj.items():
-    #         if k=='wandb_project':
-    #             dict_['project'] = v
-    #         elif k=='wandb_entity':
-    #             dict_['entity'] = v
-    #         elif k=='wandb_group':
-    #             dict_['group'] = v
-    #         elif k=='wandb_jobtype':
-    #             dict_['jobtype'] = v
-    #         elif k=='wandb_offline':
-    #             dict_['offline'] = v
-    #         elif k=='wandb':
-    #             dict_['wandb'] = v
-    #     assert dict_['wandb'] == True
-    #     return WandBSettings(**dict_)
     
     @staticmethod
     def structure_from_yaml(obj: Dict, _): # Will only enter this function if wandb field exists in yaml file
@@ -66,6 +47,7 @@ class RealmTuneBaseConfig:
     warmup_trials: int = parser.get_default('warmup_trials')
     eval_window_size: int = parser.get_default('eval_window_size')
     data_path: Union[str, None] = parser.get_default('data_path')
+    env_path: Union[str, None] = parser.get_default('env_path')
     wandb: WandBSettings = attr.ib(factory=WandBSettings)
 
     @staticmethod
@@ -81,24 +63,10 @@ class RealmTuneBaseConfig:
     #     if parser.get_default('data_path') is None:
     #         return 
 
-# class MLAgentsBaseConfig:
-#     default_settings: Dict = attr.Factory(dict)
-
-#     # From ML-Agents
-#     env_settings: EnvironmentSettings = attr.ib(factory=EnvironmentSettings)
-#     engine_settings: EngineSettings = attr.ib(factory=EngineSettings)
-#     environment_parameters: Optional[Dict[str, EnvironmentParameterSettings]] = None
-#     checkpoint_settings: CheckpointSettings = attr.ib(factory=CheckpointSettings)
-#     torch_settings: TorchSettings = attr.ib(factory=TorchSettings)
-#     debug: bool = False
-
-
 @attr.s(auto_attribs=True)
 class RealmTuneConfig:
     realm_ai: RealmTuneBaseConfig = attr.ib(factory=RealmTuneBaseConfig)
     mlagents: Dict = attr.Factory(dict)
-
-    # cattr.register_structure_hook(RealmTuneBaseConfig, RealmTuneBaseConfig.structure)
 
     @staticmethod
     def from_yaml_file(path_to_yaml_file): 
@@ -107,8 +75,26 @@ class RealmTuneConfig:
         with open(path_to_yaml_file) as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
 
-        # TODO: Ensure that all fields in realm_ai portion of yaml file are valid - we leave validation of mlagents and values of the fields to their own functions
+        # Ensure that all fields in realm_ai portion of yaml file are valid - we leave validation of mlagents and values of the fields to their own functions
+        if 'realm_ai' in config:
+            for k, v in config['realm_ai'].items():
+                assert k in attr.fields_dict(RealmTuneBaseConfig), f'"{k}" field is not supported in {path_to_yaml_file}!'
         return converter.structure(config, RealmTuneConfig)
+
+    @staticmethod
+    def from_argparse(parser: argparse.ArgumentParser):
+        args = parser.parse_args()
+        realm_tune_config = RealmTuneConfig.from_yaml_file(args.config_path)
+        dict_ = cattr.unstructure(realm_tune_config)
+        for k, v in vars(args).items():
+            if k in DetectDefault.non_default_args:
+                if k in attr.fields_dict(RealmTuneBaseConfig):
+                    dict_['realm_ai'][k] = v
+                elif k in attr.fields_dict(WandBSettings):
+                    dict_['realm_ai']['wandb'][k] = v
+                else:
+                    if k != "config_path": warnings.warn(f'"{k}" field in yaml file not supported, and will be ignored')
+        return cattr.structure(dict_, RealmTuneConfig)
 
 # TODO: Look into "if key in DetectDefault.non_default_args:" line 878, settings.py
 # The idea is that we don't want to blindly update everything based on our argparse, we want to only update our settings based on argparse if it has been changed from its default value!
@@ -116,6 +102,8 @@ class RealmTuneConfig:
 # Solution: First, update all values using those from the config file. Then, Add a custom structure hook for each type. In the hook, iterate through argparse, and check if argument has been changed from default value. If yes, change value in dictionary. Else, don't add key to dictionary. Then just do cattr.structure(...) 
 
 if __name__=='__main__':
-    args = parser.parse_args(["--config-path","realm_tune/bayes.yaml"])
-    item = RealmTuneConfig.from_yaml_file(args.config_path)
-    print(item)
+    # args = parser.parse_args(["--config-path","realm_tune/bayes.yaml"])
+    # item = RealmTuneConfig.from_yaml_file(args.config_path)
+    # print(item)
+
+    print(RealmTuneConfig.from_argparse(parser))
