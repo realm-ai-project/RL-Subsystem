c20b56bba10113c7ac6ad2784e6d4fa7244f945d
Ken Ming Lee <leekenming99@gmail.com>
2021-12-12 16:43:36 -0500

    Revert "minor refactoring"
    This reverts commit 1adcbdbacbcd2d25071b6cb91784c45e22b3c4f8.

--- realm_tune/settings.py
+++ realm_tune/settings.py
@@ -7,9 +7,12 @@ import time
 
 import attr
 import cattr
+from cattr.gen import make_dict_unstructure_fn, override
 from cattr.converters import Converter
+import wandb
+import yaml
+from mlagents_envs.environment import UnityEnvironment
 
-from realm_tune.utils import load_yaml_file, assert_singleplayer_env
 from realm_tune.cli_config import DetectDefault, parser
 
 @attr.s(auto_attribs=True)
@@ -285,6 +288,24 @@ class RealmTuneConfig:
         
         realm_tune_config = converter.structure(config, RealmTuneConfig)
         return realm_tune_config
+
+def assert_singleplayer_env(env_path):
+    _env = UnityEnvironment(env_path, no_graphics=True)
+    if not _env.behavior_specs:
+        _env.step()
+    try:
+        if len(_env.behavior_specs) != 1:
+            raise Exception(
+                "Realm Tune only works with single player environments for now"
+            )
+    finally:
+        _env.close()
+
+def load_yaml_file(path_to_yaml_file): 
+    with open(path_to_yaml_file) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    return config
 
 
 # For debugging purposes
--- realm_tune/utils.py
+++ realm_tune/utils.py
@@ -1,27 +1,8 @@
+
 from typing import Any, Mapping
 
-from mlagents_envs.environment import UnityEnvironment
-import yaml
 
 from realm_tune.settings import WandBSettings
 
 def add_wandb_config(config_dict: Mapping[str, Any], wandb_settings:WandBSettings) -> None:
     config_dict['wandb'] = wandb_settings.to_dict()
-
-def assert_singleplayer_env(env_path):
-    _env = UnityEnvironment(env_path, no_graphics=True)
-    if not _env.behavior_specs:
-        _env.step()
-    try:
-        if len(_env.behavior_specs) != 1:
-            raise Exception(
-                "Realm Tune only works with single player environments for now"
-            )
-    finally:
-        _env.close()
-
-def load_yaml_file(path_to_yaml_file): 
-    with open(path_to_yaml_file) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-
-    return config
