35b0d6a37924c28e54916a259ec7134513103ca5
Ken Ming Lee <leekenming99@gmail.com>
2022-01-06 16:22:56 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='realm_ai',
-    version='0.0.1',
+    version='0.0.4-alpha',
     description="REALM_AI's RL Subsystem",
     install_requires=[
         "requests",  
